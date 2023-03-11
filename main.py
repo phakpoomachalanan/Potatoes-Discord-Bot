3e96836d8a48ded6982a304cd008ee1688eeddb3
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-03-11 13:36:06 +0700

    add condition to chatgpt response

--- main.py
+++ main.py
@@ -14,27 +14,32 @@ CHATGPT_CHANNEL_ID = getenv("CHATGPT_CHANNEL_ID")
 client = discord.Client(intents=discord.Intents(members=True, message_content=True, guild_messages=True, guild_reactions=True, guilds=True))
 openai.api_key = OPENAI_API_KEY
 
-async def textToCode(message):
+async def text_to_code(message):
+    user = message.autho
     msg = message.content
     lang = msg.split()[0]
-    user = message.author
+    code = msg[len(lang):]
 
-    await message.channel.send(f"```{lang}\nFrom: {user}\n\n{msg[len(lang):]}```")
+    await message.channel.send(f"```{lang}\nFrom: {user}\n\n{code}```")
     await message.delete()
 
     return
 
-async def askChatGpt(message):
+async def ask_chat_gpt(message):
     msg = message.content
+
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages = [
-            {"role": "user", "content": msg}
+            {"role": "user", "content": msg + " please make response message under 2000 letters"}
         ]
     )
 
-    if response["choices"][0]["finish_reason"] != "null":
-        await message.channel.send(response["choices"][0]["message"]["content"], reference=message)
+    res_choices = response["choices"][0]
+    res_msg = res_choices["message"]["content"]
+
+    if res_choices["finish_reason"] != "null":
+        await message.channel.send(res_msg, reference=message)
     else:
         await message.channel.send("API request failed", reference=message)
 
@@ -52,8 +57,8 @@ async def on_message(message):
     msg_channel = str(message.channel.id)
 
     if msg_channel == CODE_CHANNEL_ID:
-        textToCode(message)
+        text_to_code(message)
     elif msg_channel == CHATGPT_CHANNEL_ID:
-        await askChatGpt(message)
+        await ask_chat_gpt(message)
 
 client.run(TOKEN)
