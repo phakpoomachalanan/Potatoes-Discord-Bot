96bad0621a6bd0891e8ed4b6f59c6890e7a65803
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-09-03 21:45:32 +0700

    change if statements

--- module/sh_command.py
+++ module/sh_command.py
@@ -9,9 +9,7 @@ async def command(message):
         if (sh_script == "git pull"):
             sh_script = "git --work-tree=/home/popupie/pupserver/ZideQuest-Backend --git-dir=/home/popupie/pupserver/ZideQuest-Backend/.git pull".split()
             # sh_script = "git --work-tree=/Users/puppie/Code/Potatoes-Discord-Bot --git-dir=/Users/puppie/Code/Potatoes-Discord-Bot/.git pull".split()
-            result = subprocess.run(sh_script, stdout=subprocess.PIPE).stdout.decode("utf-8")
-        else:
-            result = subprocess.run(sh_script, stdout=subprocess.PIPE).stdout.decode("utf-8")
+        result = subprocess.run(sh_script, stdout=subprocess.PIPE).stdout.decode("utf-8")
         if (len(result) == 0):
             result = "No output"
         await dis.send_msg(message, f'```{result}```')
