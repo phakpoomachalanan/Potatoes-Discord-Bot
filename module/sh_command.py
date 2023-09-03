b1f9841732a85decd7bb2eac08884647890b6416
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-09-03 22:36:48 +0700

    catch error and restart

--- module/sh_command.py
+++ module/sh_command.py
@@ -1,6 +1,7 @@
 import subprocess
 import json
-from os import getenv
+import sys
+from os import getenv, execv
 from dotenv import load_dotenv
 import module.dis_com as dis
 
@@ -12,9 +13,15 @@ async def command(message):
     if (msg_content.startswith("$")):
         sh_script = msg_content.strip("$ ")
         if (sh_script[:8] == "git pull"):
-            path = GIT_PATH[sh_script.split(", ")[1]]
+            try:
+                path = GIT_PATH[sh_script.split(", ")[1]]
+            except KeyError:
+                await dis.send_msg(message, f'```Cannot find git repo```')
             sh_script = f"git --work-tree={path} --git-dir={path}/.git pull".split()
         result = subprocess.run(sh_script, stdout=subprocess.PIPE).stdout.decode("utf-8")
         if (len(result) == 0):
             result = "No output"
         await dis.send_msg(message, f'```{result}```')
+    elif (msg_content == "restart"):
+        await dis.send_msg(message, '```Restarting```')
+        execv(sys.executable, ['python3'] + sys.argv)
