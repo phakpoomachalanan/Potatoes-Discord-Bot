5be484f584f85f4372a38569c5a68e5dd5e1b4b7
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-09-04 14:53:58 +0700

    add hot reload when pull

--- module/sh_command.py
+++ module/sh_command.py
@@ -10,18 +10,22 @@ GIT_PATH = json.loads(getenv("GIT_PATH"))
 
 async def command(message):
     msg_content = message.content
+    temp = str()
     if (msg_content.startswith("$")):
         sh_script = msg_content.strip("$ ")
         if (sh_script[:8] == "git pull"):
             try:
-                path = GIT_PATH[sh_script.split(", ")[1]]
+                temp = sh_script.split(", ")[1]
+                path = GIT_PATH[temp]
             except KeyError:
                 await dis.send_msg(message, f'```Cannot find git repo```')
             sh_script = f"git --work-tree={path} --git-dir={path}/.git pull".split()
-        result = subprocess.run(sh_script, stdout=subprocess.PIPE).stdout.decode("utf-8")
+        result = subprocess.run(sh_script.split(), stdout=subprocess.PIPE).stdout.decode("utf-8")
         if (len(result) == 0):
             result = "No output"
         await dis.send_msg(message, f'```{result}```')
+        if (temp == "Potato"):
+            execv(sys.executable, ['python3'] + sys.argv)
     elif (msg_content == "restart"):
         await dis.send_msg(message, '```Restarting```')
         execv(sys.executable, ['python3'] + sys.argv)
