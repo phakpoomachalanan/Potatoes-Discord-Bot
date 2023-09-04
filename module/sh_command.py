a6298917aa3fea25001b0e419d19e4ac07506046
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-09-04 15:00:30 +0700

    startswith

--- module/sh_command.py
+++ module/sh_command.py
@@ -20,7 +20,7 @@ async def command(message):
             except KeyError:
                 await dis.send_msg(message, f'```Cannot find git repo```')
             sh_script = f"git --work-tree={path} --git-dir={path}/.git pull".split()
-        elif (sh_script.startwith("rm")):
+        elif (sh_script.startswith("rm")):
             return
         result = subprocess.run(sh_script.split(), stdout=subprocess.PIPE).stdout.decode("utf-8")
         if (len(result) == 0):
