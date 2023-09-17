f9fbefc79b496d6653f3605f4c3210df6fa94643
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-09-17 21:53:54 +0700

    commented crazy

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ async def on_message(message):
 
     if (msg_content.startswith("//")):
         return
-    if ("crazy" in msg_content.lower()):
-        await message.channel.send(f"crazy? I was crazy once.", reference=message)
+    # if ("crazy" in msg_content.lower()):
+    #     await message.channel.send(f"crazy? I was crazy once.", reference=message)
     if (msg_channel == CODE_CHANNEL_ID):
         await ttc.text_to_code(message)
     elif (msg_channel == CHATGPT_CHANNEL_ID):
